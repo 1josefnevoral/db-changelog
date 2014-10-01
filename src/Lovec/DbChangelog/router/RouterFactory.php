c08c5482ff3914a0da485a9f55308f29c2d3d2e0
Tomas Votruba <info@tomasvotruba.cz>
2014-10-01 13:05:11 +0200

    RouterFactory license added

--- src/Lovec/DbChangelog/router/RouterFactory.php
+++ src/Lovec/DbChangelog/router/RouterFactory.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the DbChangelog package
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
 namespace Lovec\DbChangelog\Router;
 
 use Nette\Application\Routers\Route;
